91315172f56c95d54e1da78bda16b5f5a56803cb
log1NVolcom <joaorego_5@hotmail.com>
2019-03-29 23:54:03 +0000

    [bug] env

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 const app = express();
+var cors = require('cors')
 
 dotenv.config();
 //Body parser midleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
+
+vconst corsOptions = {
+    origin: process.env.ENDPOINT,
+    optionsSuccessStatus: 200
+};
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
