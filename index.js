2fa213703a211053bad47244511c32444d6a80be
log1NVolcom <joaorego_5@hotmail.com>
2019-05-04 22:59:41 +0100

    revert

--- index.js
+++ index.js
@@ -10,27 +10,18 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var whitelist = [process.env.ENDPOINT, process.env.ENDPOINT2];
 
-var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+const corsOptions = {
+    origin: process.env.ENDPOINT,
     optionsSuccessStatus: 200
-}
-
+};
 app.use(cors(corsOptions));
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log('Server started...'));
-app.options('*', cors());
 
-app.post('/api/mail/send', cors(), async (req, res) => {
+app.post('/api/mail/send', async (req, res) => {
     let account = await nodemailer.createTestAccount();
     const user = process.env.GMAIL_USER;
     const pass = process.env.GMAIL_PASSWORD;
